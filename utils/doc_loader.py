54e827067c82cb69927aff80d1ea65d727120549
Mahmud Hasan Munna <mhmunnamm27@gmail.com>
2026-05-31 18:25:24 +0600

    added folders

---
+++ utils/doc_loader.py
@@ -0,0 +1,11 @@
+## DocLoader
+from langchain_community.document_loaders import PyMuPDFLoader
+
+def resume_loader(file_path):
+    loader = PyMuPDFLoader(file_path)
+    doc_list =  loader.load()
+    return '\n'.join([i.page_content for i in doc_list if i.page_content])
+
+def text_loader(file_path):
+    with open(file_path, 'r') as f:
+        return f.read()
