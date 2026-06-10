eeb1c6fd26d5ea79f0b7488b2d23c490c90e35f1
Mahmud Hasan Munna <mhmunnamm27@gmail.com>
2026-06-10 17:13:51 +0600

    add test endpoint to FastAPI application

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 from routes.api import router
 
 app = FastAPI(title="CareerPilot API")
+
+@app.get('/')
+def test():
+    return "Hello"
+
 app.include_router(router)
 
 
